68b2b3254c7ae592923c42a0f28b9ea37b7e23e6
Florian <florian.heslouin@bibliosansfrontieres.org>
2019-02-01 14:35:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AnsibleCube
+
+AnsibleCube aim to automatically deploy the [Ideascube plateform](http://github.com/ideascube/ideascube/) and download several types of content \(ZIM files, Khan Academy videos, Ideascube packages\).
+
+Head to [AnsibleCube handbook](http://ansiblecube.doc.bibliosansfrontieres.org) for more setup & configuration details
---
+++ ideascube_setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+ANSIBLECAP_PATH="/var/lib/ansible/local"
+GIT_REPO_URL="https://github.com/bibliosansfrontieres/ansiblecap.git"
+ANSIBLE_BIN="/usr/bin/ansible-pull"
+ANSIBLE_ETC="/etc/ansible/facts.d/"
+BRANCH="master"
+
+[ $EUID -eq 0 ] || {
+    echo "Error: you have to be root to run this script." >&2
+    exit 1
+}
+
+function internet_check()
+{
+    echo -n "[+] Check Internet connection... "
+    if [[ ! `ping -q -c 2 github.com` ]]
+    then
+        echo "ERROR: Repository is unreachable, check your Internet connection." >&2
+        exit 1
+    fi
+    echo "Done."
+}
+
+function install_ansible()
+{
+    echo -n "[+] Updating APT cache... "
+    internet_check
+    apt-get update --quiet --quiet
+    echo 'Done.'
+
+    echo -n "[+] Install ansible... "
+    apt-get install --quiet --quiet -y software-properties-common git lsb-release
+    apt-add-repository --yes --update ppa:ansible/ansible
+    apt-get install --quiet --quiet -y ansible
+    echo 'Done.'
+}
+
+function clone_ansiblecube()
+{
+    echo -n "[+] Checking for internet connectivity... "
+    internet_check
+    echo 'Done.'
+
+    echo -n "[+] Clone ansiblecap repo... "
+    mkdir --mode 0755 -p ${ANSIBLECAP_PATH}
+    cd ${ANSIBLECAP_PATH}/../
+    git clone ${GIT_REPO_URL} local
+
+    mkdir --mode 0755 -p ${ANSIBLE_ETC}
+    cp ${ANSIBLECAP_PATH}/hosts /etc/ansible/hosts
+    echo 'Done.'
+}
+
+[ -x /usr/bin/ansible ] || install_ansible
+[ -d ${ANSIBLECAP_PATH} ] || clone_ansiblecube
+
+echo "Checking file access" >> /var/log/ansible-pull.log
+[ $? -ne 0 ] && echo "No space left to write logs or permission problem, exiting." && exit 1
+
+cd $ANSIBLECAP_PATH
+
+echo "$ANSIBLE_BIN -C $BRANCH -d $ANSIBLECAP_PATH -i hosts -U $GIT_REPO_URL main.yml --extra-vars @device.json" >> /var/lib/ansible/ansible-pull-cmd-line.sh
+echo -e "[+] Start configuration...follow logs : tail -f /var/log/ansible-pull.log"
+
+$ANSIBLE_BIN -C $BRANCH -d $ANSIBLECAP_PATH -i hosts -U $GIT_REPO_URL main.yml --extra-vars "@device.json"
