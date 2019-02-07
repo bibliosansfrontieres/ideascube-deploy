1484591751ae7ec554ff715212952e621e336e59
Florian <florian.heslouin@bibliosansfrontieres.org>
2019-02-07 12:31:49 +0100

    Remove for testing purpose

--- ideascube_setup.sh
+++ ideascube_setup.sh
@@ -92,4 +92,5 @@ wget https://github.com/bibliosansfrontieres/ideascube-deploy/raw/master/device.
 echo "$ANSIBLE_BIN -C $GIT_RELEASE_TAG -d $ANSIBLECAP_PATH -i hosts -U $GIT_REPO_URL main.yml --extra-vars @device.json $TAG" >> /var/lib/ansible/ansible-pull-cmd-line.sh
 echo -e "[+] Start configuration...follow logs : tail -f /var/log/ansible-pull.log"
 
-$ANSIBLE_BIN -C $GIT_RELEASE_TAG -d $ANSIBLECAP_PATH -i hosts -U $GIT_REPO_URL main.yml --extra-vars "@device.json" $TAG
+# $ANSIBLE_BIN -C $GIT_RELEASE_TAG -d $ANSIBLECAP_PATH -i hosts -U $GIT_REPO_URL main.yml --extra-vars "@device.json" $TAG
+$ANSIBLE_BIN -d $ANSIBLECAP_PATH -i hosts -U $GIT_REPO_URL main.yml --extra-vars "@device.json" $TAG
