87822b9ea2c27977f4404e0419c7b8ba5b8bdc75
Florian <florian.heslouin@bibliosansfrontieres.org>
2019-02-07 15:46:28 +0100

    Add some coment

--- reset.sh
+++ reset.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+# This script soft reset the CAP and format the HHD
+
+# Erase the current configuration with a backup
 cd / && tar xvf /etc/config-backup.tar.gz
 
 r2f=`uci get system.@system[0].r2f`
 
 [ "$r2f" = 0 ] && uci set system.@system[0].r2f=1 && uci commit system
 
-# get eth first mac
+# get eth first mac and get the laste 2 numbers
 addr=`find /sys/class/net/ | grep -m1 'enp2s0'`
 emac=`cat $addr/address | awk -F":" '{ print $5 $6 }'`
 
-# hostname
+# Set a default hostname
 echo "CMAL-$emac" > /etc/hostname
 sed -i -e "s,$(cat /etc/hosts | grep 127.0.1.1 | awk -F" " '{ print $2 }'),CMAL-$emac,g" /etc/hosts
 
@@ -18,7 +21,7 @@ sed -i -e "s,$(cat /etc/hosts | grep 127.0.1.1 | awk -F" " '{ print $2 }'),CMAL-
 uci set system.@system[0].hostname=my.content
 uci commit system
 
-# ssid
+# Restore the ssid
 uci set wireless.@wifi-iface[0].ssid=CMAL-2.4G-$emac
 uci set wireless.@wifi-iface[1].ssid=CMAL-5G-$emac
 
@@ -26,7 +29,11 @@ uci commit wireless
 
 sync
 
+# Force umount the HDD
 umount -A -l /dev/sda1
+
+# Format the partition
 mkfs.ext4 /dev/sda1
 
+# Shutdown the device
 sudo poweroff
