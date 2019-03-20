370a89dc4344feeabc6a4d5fec10b83caf9d1405
Florian <florian.heslouin@bibliosansfrontieres.org>
2019-03-20 15:22:20 +0100

    Install missing package jq

--- ideascube_setup.sh
+++ ideascube_setup.sh
@@ -99,6 +99,7 @@ do
 done
 
 echo -n "[+] Retrieve device configuration from API"
+apt-get install --quiet --quiet -y jq
 curl -vs http://10.90.100.254:1337/projects?project_name=$PROJECT_NAME |jq ".[]" > /etc/ansible/facts.d/device_configuration.json
 
 cd $ANSIBLECAP_PATH
