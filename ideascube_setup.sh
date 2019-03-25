7e19d9f486f566ec1a20a251392c98ec0599cf62
Florian <florian.heslouin@bibliosansfrontieres.org>
2019-03-25 15:24:12 +0100

    Test whether we are inside or outside bsf local network

--- ideascube_setup.sh
+++ ideascube_setup.sh
@@ -100,7 +100,12 @@ done
 
 echo -n "[+] Retrieve device configuration from API"
 apt-get install --quiet --quiet -y jq
-curl -vs http://10.90.100.254:1337/projects?project_name=$PROJECT_NAME |jq ".[]" > /etc/ansible/facts.d/device_configuration.json
+if [[ ! `ping -q -c 2 10.10.9.1` ]]
+then
+  curl -vs http://10.90.100.254:1337/projects?project_name=$PROJECT_NAME |jq ".[]" > /etc/ansible/facts.d/device_configuration.json
+else
+  curl -vs http://10.10.9.38:1337/projects?project_name=$PROJECT_NAME |jq ".[]" > /etc/ansible/facts.d/device_configuration.json
+fi
 
 cd $ANSIBLECAP_PATH
 
