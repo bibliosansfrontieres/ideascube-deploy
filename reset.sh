7e19e398024dbb2c43edd6ae290a063b0a0fe4e6
Florian <florian.heslouin@bibliosansfrontieres.org>
2019-05-07 14:39:53 +0200

    Clean up tinc install and files called at start-up

--- reset.sh
+++ reset.sh
@@ -12,8 +12,19 @@ emac=`cat $addr/address | awk -F":" '{ print $5 $6 }'`
 # Set a default hostname
 echo "CMAL-$emac" > /etc/hostname
 
+# Remove files executed at start up
+for files in 45-pull-containers 49-cache-server 50-update-content 60-push-log
+do
+   rm -f /etc/hotplug.d/iface/$files
+done
+
 # Set back old password
 usermod --password '$1$.SwYxBkA$sIY5tCkbXGeK/cl/VcnRf0' cap
+
+# Disable tinc
+systemctl stop tinc@testvpn.service
+systemctl disable tinc@testvpn.service
+rm -rf /etc/tinc/
 
 # Clean up balena-engine data
 systemctl stop balena-engine
