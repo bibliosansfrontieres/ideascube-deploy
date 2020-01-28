b16ae663c92fbe30fbd39abcdd06d801a5357d9b
Florian <florian.heslouin@bibliosansfrontieres.org>
2020-01-28 18:16:51 +0100

    Bump 1.6.1

--- ideascube_setup.sh
+++ ideascube_setup.sh
@@ -6,7 +6,7 @@ ANSIBLE_BIN="/usr/bin/ansible-pull"
 ANSIBLE_ETC="/etc/ansible/facts.d/"
 TAGS=""
 BRANCH="master"
-GIT_RELEASE_TAG="1.6"
+GIT_RELEASE_TAG="1.6.1"
 
 [ $EUID -eq 0 ] || {
     echo "Error: you have to be root to run this script." >&2
